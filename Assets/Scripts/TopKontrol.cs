106af445852dd2a6eda8de2933635df3f3c668cb
Computer-Worm <yusufhasarr@gmail.com>
2023-06-09 20:03:30 +0300

    Objenin Hareketi

---
+++ Assets/Scripts/TopKontrol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopKontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(0, 0, 0.01f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, 0.01f);
+    }
+}
