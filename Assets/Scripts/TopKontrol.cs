a12bee7d7054228671176c69ea2fdac52c8dd160
Computer-Worm <yusufhasarr@gmail.com>
2023-06-13 21:14:28 +0300

    Topun zıplaması ve asetst değişiklikleri

--- Assets/Scripts/TopKontrol.cs
+++ Assets/Scripts/TopKontrol.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TopKontrol : MonoBehaviour
 {
     public Rigidbody rb;
     public int Speed;
+    public float jumpSpeed;
+
     public int puan = 0;
     public int objeSayisi;
 
     public Text puanText;
     public Text oyunBittiText;
+
+    public Button RestartButton;
+
+    [SerializeField] private AudioSource point;
 
     void Start()
     {
@@ -27,6 +34,17 @@ public class TopKontrol : MonoBehaviour
 
         rb.AddForce(vektor * Speed);
 
+        if (transform.position.y < 3.20)
+        {
+            //Debug.Log("Sınır Aşıldı..");    
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+            }
+        }
+
+
     }
     
     void  OnTriggerEnter(Collider other)
@@ -35,10 +53,17 @@ public class TopKontrol : MonoBehaviour
         puan += 5;
 
         puanText.text = "Puan: " + puan;
+        point.Play();
 
         if (puan == objeSayisi)
         {
             oyunBittiText.gameObject.SetActive(true);
+            RestartButton.gameObject.SetActive(true);
         }
+    }
+
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
